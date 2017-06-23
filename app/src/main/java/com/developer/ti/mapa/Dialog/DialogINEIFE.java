fb5af7b54f95b1fd6acfe7aebbfc0331baa6b564
tecnico airmovil <tecnicoairmovil@INFINITUMAR43GR.local>
2017-06-22 20:50:58 -0500

    inclucion de seccion de identificacion oficial

--- app/src/main/java/com/developer/ti/mapa/Dialog/DialogINEIFE.java
+++ app/src/main/java/com/developer/ti/mapa/Dialog/DialogINEIFE.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -13,12 +14,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,31 +37,46 @@ import com.developer.ti.mapa.R;
 import com.developer.ti.mapa.tools.RequestPermissionsTool;
 import com.developer.ti.mapa.tools.RequestPermissionsToolImpl;
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.IOException;
 
 
 /**
  * Created by tecnicoairmovil on 21/06/17.
  */
 
-public class DialogINEIFE extends DialogFragment implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback{
+public class DialogINEIFE extends DialogFragment implements View.OnClickListener{
     private static final String TAG = DialogINEIFE.class.getSimpleName();
     private Dialog dialog;
     private View rootView;
-    private ImageView _ivIneIfe;
-    private Button _btnValidate;
-    static final int PHOTO_REQUEST_CODE = 1;
-    private static int RESULT_LOAD_IMAGE = 1;
-    private int RESULT_OK = -1;
-    private static final int CAMERA_REQUEST = 1888;
-
-    SurfaceView cameraView;
-    TextView textView;
+    private SurfaceView cameraView;
+    private TextView textView;
     CameraSource cameraSource;
-    final int RequesCameraPermissionID = 1001;
+    final int RequestCameraPermissionID = 1001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case RequestCameraPermissionID: {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -82,37 +101,73 @@ public class DialogINEIFE extends DialogFragment implements View.OnClickListener
         rootView = view;
         init();
 
-        cameraView = (SurfaceView) rootView.findViewById(R.id.surfaceview);
-        textView = (TextView) rootView.findViewById(R.id.text1);
+        cameraView = (SurfaceView) rootView.findViewById(R.id.surface);
+        textView = (TextView) rootView.findViewById(R.id.text_view);
 
-
-
-
-        /*        Button captureImg = (Button) rootView.findViewById(R.id.action_btn);
-        if (captureImg != null) {
-            captureImg.setOnClickListener(new View.OnClickListener() {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getActivity().getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            Log.e(TAG, "El detector de dependencias no esta habilitado !!");
+        } else {
+            cameraSource = new CameraSource.Builder(getActivity().getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
                 @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(
-                            Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                public void surfaceCreated(SurfaceHolder holder) {
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+                            return;
+                        }
+                        cameraSource.start(cameraView.getHolder());
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
 
-                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                    Log.e(TAG, "--> holder: " + holder + "\nformat: " + format + "\nwidth: " + width + "\nheight: " + height);
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0){
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i = 0; i < items.size(); i++){
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+
+                                }
+
+                                textView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
                 }
             });
         }
-        */
 
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            _ivIneIfe.setImageBitmap(photo);
-
-
-        }
     }
 
     @Override
@@ -130,21 +185,10 @@ public class DialogINEIFE extends DialogFragment implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.text_view_validate:
-                Snackbar snackbar = Snackbar.make(v, "Espere un momento...", Snackbar.LENGTH_LONG).setAction("Action", null);
-                ColoredSnackBar.confirm(snackbar);
-                snackbar.show();
-                break;
-        }
+
     }
-
-
-
     private void init(){
-        //_ivIneIfe = (ImageView) rootView.findViewById(R.id.image_view_ine_ife);
-        _btnValidate= (Button) rootView.findViewById(R.id.text_view_validate);
-        _btnValidate.setOnClickListener(this);
+
     }
 
 }
